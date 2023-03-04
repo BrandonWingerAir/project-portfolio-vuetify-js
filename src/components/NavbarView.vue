c215eb8e684e28599b35f7a6931699bdc0a46502
Brandon Winger-Air <brandonwinger-air@outlook.com>
2023-03-04 15:08:06 -0500

    setup navbar scroll and section styles

--- src/components/NavbarView.vue
+++ src/components/NavbarView.vue
@@ -1,18 +1,23 @@
 <template>
     <v-app-bar app color="#111111" dark flat class="px-8">
         <v-btn icon>
-            <v-icon color="#00b8d4">fa-brands fa-wolf-pack-battalion</v-icon>
+            <v-icon color="#039be5    ">fa-brands fa-wolf-pack-battalion</v-icon>
         </v-btn>
         <v-spacer></v-spacer>
-        <v-btn text>Home</v-btn>
-        <v-btn text>Projects</v-btn>
-        <v-btn text>About</v-btn>
-        <v-btn text>Contact</v-btn>
+        <v-btn text @click="scroll('home')">Home</v-btn>
+        <v-btn text @click="scroll('projects')">Projects</v-btn>
+        <v-btn text @click="scroll('about')">About</v-btn>
+        <v-btn text @click="scroll('contact')">Contact</v-btn>
     </v-app-bar>
 </template>
 
 <script>
     export default {
-        
+        methods: {
+            scroll(refName) {
+                const element = document.getElementById(refName);
+                element.scrollIntoView({ behavior: "smooth" });
+            }
+        }
     }
 </script>
