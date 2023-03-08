cc01db6cdd7a20a2850dddfd26cc9a9c313c9d24
Brandon Winger-Air <brandonwinger-air@outlook.com>
2023-03-08 17:56:05 -0500

    page title configuration

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
+  pages: {
+    index: {
+        entry: 'src/main.js',
+        title: 'Portfolio | Brandon Winger-Air'
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ]
