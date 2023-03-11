d8b592c9fd60cd039d0e626a956e8763291fff72
Brandon Winger-Air <brandonwinger-air@outlook.com>
2023-03-10 23:49:55 -0500

    update navbar responsive styling

--- src/components/NavbarSection.vue
+++ src/components/NavbarSection.vue
@@ -1,10 +1,10 @@
 <template>
-    <v-app-bar app color="#111111" dark flat class="px-8" max-width="100vw">
+    <v-app-bar app color="#111111" dark flat class="px-8 d-sm-inline-block d-flex" max-width="100vw">
         <v-btn icon>
             <v-icon color="#039be5">fa-solid fa-globe</v-icon>
         </v-btn>
-        <v-spacer class="xs-hide"></v-spacer>
-        <v-btn text @click="scroll('home')" class="xxs-hide">Home</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn text @click="scroll('home')" class="xs-hide">Home</v-btn>
         <v-btn text @click="scroll('projects-scroll')">Projects</v-btn>
         <v-btn text @click="scroll('about-scroll')">About</v-btn>
         <v-btn text @click="scroll('contact')">Contact</v-btn>
@@ -25,35 +25,20 @@
 <style scoped>
 @media only screen and (max-width: 600px) {
     .v-toolbar {
-        padding-left: 4vw !important;
+        justify-content: center !important;
     }
 
-    button {
-        padding-left: 4vw !important;
-        padding-right: 4vw !important;
-    }
+}
 
+@media only screen and (max-width: 480px) {
     .xs-hide {
         display: none;
     }
-
-    
 }
 
-@media only screen and (max-width: 500px) {
-    .v-toolbar {
-        padding-left: 2vw !important;
-    }
-
-    button {
-        padding-left: 3vw !important;
-        padding-right: 3vw !important;
-    }
-}
-
-@media only screen and (max-width: 400px) {
-    .xxs-hide {
-        display: none;
+@media only screen and (max-width: 380px) {
+    .v-btn {
+        padding: 8px !important;
     }
 }
 </style>
